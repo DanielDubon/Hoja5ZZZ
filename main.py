46d5e93d8b2f718e770928fdb80b905d6a4210da
DanielDubon <dub22233@uvg.edu.gt>
2023-02-26 14:30:41 -0600

    Software simluation completed

--- main.py
+++ main.py
@@ -2,72 +2,86 @@ import random
 import simpy
 import statistics
 
+time_running = []
+totaltime = 0
+
 
 class OS(object):
 
-    def __init__(self, env, ram_available, cpu_cores, instructions):
+    def __init__(self, env, ram_available, cpu_cores):
         self.env = env
         self.ram_available = simpy.Container(env, init=ram_available, capacity=ram_available)
-        self.cpu_cores = simpy.Resource(cpu_cores)
-        self.instructions = instructions
+        self.cpu_cores = simpy.Resource(env, cpu_cores)
+        self.cpu_queue = simpy.Resource(env, cpu_cores)
 
-    def new(self, process):
-        print("Asignacion de RAM a proceso" + str(process) + " en t= " + str(env.now))
-
-        if self.ram_available.level == 0:
-            yield self.ram_available.get(random.randint(1, 11))
-            print("Se asigno la RAM")
-            yield self.ready(process)
-
-        else:
-            while self.ram_available.capacity == 0:
-                print("Esperando a que la ram se libere...")
-
-    def ready(self, process):
-        print("El proceso " + str(process) + " esta listo para ser ejecutado en t= " + str(env.now))
-        yield self.running(process)
-
-    def running(self, process):
-        print("El proceso " + str(process) + " se esta ejecutando en t= " + str(env.now))
-        remaining_instructions = self.instructions
-        while remaining_instructions > 0:
-            with self.cpu_cores.request() as request:
-                yield request
-                if remaining_instructions < 3:
-                    yield self.env.timeout(1)
-                    remaining_instructions -= remaining_instructions
-                else:
-                    yield self.env.timeout(1)
-                    remaining_instructions -= 3
-
-            # Determine whether the process terminates, waits, or returns to ready
-            if remaining_instructions == 0:
-                print("Proceso " + str(process) + " terminado en t=" + str(env.now))
-            else:
-                io_or_ready = random.randint(1, 21)
-                if io_or_ready == 1:
-                    print("Proceso " + str(process) + " en espera de I/O en t=" + str(env.now))
-                    yield self.env.timeout(1)
-                    yield self.ready(process)
-                elif io_or_ready == 2:
-                    print("Proceso " + str(process) + " regresa a estado listo en t=" + str(env.now))
-                    yield self.ready(process)
-
-    def Software_usage(self, process):
-        ram_needed = random.randint(1, 11)
+    def Software_usage(self, process, instructions, ram_needed):
+        global totaltime
+        yield env.timeout(random.expovariate(1.0 / 10))
+        total_time = env.now
+        # Ejecucion NEW
         print("Process {} needs {} units of RAM".format(process, ram_needed))
         yield self.ram_available.get(ram_needed)
         print("Process {} got {} units of RAM at time {}".format(process, ram_needed, self.env.now))
-        yield self.env.process(self.new(process))
+
+        # Ejecucion Ready
+        print("Process {} has {} number of instructions".format(process, instructions))
+
+        # Ejecucion running
+        instructionsDone = 0
+        instructionsLeft = instructions
+        cpu_speed = 3
+
+        while instructionsLeft != 0:
+            with self.cpu_cores.request() as request:
+                yield request
+
+            if cpu_speed > instructionsLeft:
+                instructionsDone = instructionsDone + instructionsLeft
+                instructionsLeft = 0
+            else:
+                instructionsLeft = instructionsLeft - cpu_speed
+                instructionsDone = instructionsDone + cpu_speed
+
+            print("Process {}  has finished {} of {} instructions until this cpu clock cycle at t = {}".format(process,
+                                                                                                               instructionsDone,
+                                                                                                               instructions,
+                                                                                                               env.now))
+            yield env.timeout(1)
+
+            finished_status = random.randint(1, 2)
+            if finished_status == 1 and (instructionsLeft != 0):
+                print("Process {} got the status WAITING".format(process))
+                with self.cpu_queue.request() as request:
+                    yield request
+                    yield env.timeout(1)
+            elif finished_status == 2 and (instructionsLeft != 0):
+                print("Process {} got the status Ready".format(process))
+
+        #Returning RAM
+
+        yield self.ram_available.put(ram_needed)
+        print("Process {} returned {} of RAM".format(process,ram_needed))
+        totaltime += env.now - total_time
+        time_running.append(totaltime)
 
 
+# Configuracion de simulacion
 env = simpy.Environment()
-OperativeSystem = OS(env, 100, 1, 10)
-processes = 100
+ram_available = 100
+cpu_cores = 1
+OperativeSystem = OS(env, ram_available, cpu_cores)
+random.seed(10)
+
+# Ejecucion de simulacion
+processes = 20
 for i in range(processes):
-    env.process(OperativeSystem.Software_usage(i))
+    instructions = random.randint(1, 10)
+    ram_needed = random.randint(1, 10)
+    env.process(OperativeSystem.Software_usage(i, instructions, ram_needed))
+env.run()
+Mean_time = statistics.mean(time_running)
+Stdev = statistics.stdev(time_running)
 
-
-env.process(OperativeSystem.Software_usage(2))
-env.run(until=20)
+print("MEAN TIME: " + str(Mean_time))
+print("STANDARD DEVIATION " + str(Stdev))
 
